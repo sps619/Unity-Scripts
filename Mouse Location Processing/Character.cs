ac5b6c85d15299dee680d902a7f5695048d173e1
Shubham Thakur <shubham-thakur@live.com>
2019-06-14 15:41:28 +0530

    Added Mouse Location Tracker Script

---
+++ Mouse Location Processing/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{   float colliderHalfWidth, colliderHalfHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x/2;
+        colliderHalfHeight = collider.size.y/2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // convert mouse position to world position
+        Vector3 position = Input.mousePosition;
+        position.z = -Camera.main.transform.position.z;
+        position = Camera.main.ScreenToWorldPoint(position);
+        
+        // Moving the game object
+        gameObject.transform.position = position;
+        ClampInScreen();
+    }
+    void ClampInScreen()
+    {
+        //Clamps the position of the character
+        Vector3 position = transform.position;
+        if(position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if(position.x + colliderHalfHeight > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+        if(position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+        else if(position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
+        transform.position = position;
+    }
+}
---
+++ Mouse Location Processing/ScreenUtils.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScreenUtils
+{
+    #region Fields
+    // For Boundary Checking
+    static float screenLeft, screenRight, screenTop, screenBottom;
+
+    #endregion
+
+    #region  properties
+    // <summary>
+    // Gets the left edge of the screen
+    // </summary>
+    // <value>left edge of the screen</value>
+    public static float ScreenLeft
+    {
+        get { return screenLeft; }
+    }
+    // <summary>
+    // Gets the right edge of the screen
+    // </summary>
+    // <value> right edge of the screen</value>
+    public static float ScreenRight
+    {
+        get { return screenRight; }
+    }
+    // <summary>
+    // Gets the top edge of the screen
+    // </summary>
+    // <value>top edge of the screen</value>
+    public static float ScreenTop
+    {
+        get { return screenTop; }
+    }
+    // <summary>
+    // Gets the bottom edge of the screen
+    // </summary>
+    // <value>bottom edge of the screen</value>
+    public static float ScreenBottom
+    {
+        get { return screenBottom; }
+    }
+
+    #endregion
+    
+    #region Methods
+    // <summary>
+    // Initialises the screen utilities
+    public static void Initialize()
+{
+// save screen edges in world coordinates
+		float screenZ = -Camera.main.transform.position.z;
+		Vector3 lowerLeftCornerScreen = new Vector3(0, 0, screenZ);
+		Vector3 upperRightCornerScreen = new Vector3(
+			Screen.width, Screen.height, screenZ);
+		Vector3 lowerLeftCornerWorld = 
+			Camera.main.ScreenToWorldPoint(lowerLeftCornerScreen);
+		Vector3 upperRightCornerWorld = 
+			Camera.main.ScreenToWorldPoint(upperRightCornerScreen);
+		screenLeft = lowerLeftCornerWorld.x;
+		screenRight = upperRightCornerWorld.x;
+		screenTop = upperRightCornerWorld.y;
+		screenBottom = lowerLeftCornerWorld.y;
+}
+#endregion
+}
